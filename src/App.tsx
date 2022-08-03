36fc4b0eeb641febc349a92825bd5083e9a05c0f
Bojana Bojic <bojic@work.co>
2022-08-03 16:04:52 +0200

    Add grid helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'light-react-grid/dist/index.css'
+import {Column, Grid, useBreakpoint} from "light-react-grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const breakpoint = useBreakpoint();
+
+  return <div>
+    <h2>Hello!</h2>
+  </div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import {GRID_SETTINGS, GridSystem} from "light-react-grid";
+
+ReactDOM.render(
+    <GridSystem useGridHelper={true} settings={{
+        ...GRID_SETTINGS,
+        gridHelperColumnColor: 'rgba(0, 0, 0, .05)',
+        gridHelperMargins: {
+            sm: '5%',
+            md: '5%',
+            lg: '5%'
+        },
+    }}>
+        <App/>
+    </GridSystem>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
