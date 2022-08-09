7b6efd255ab101cba82d8dd4efc7b55f3f11fbad
Bojana Bojic <bojic@work.co>
2022-08-09 11:17:15 +0100

    Refactored grid settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GRID_SETTINGS, GridSystem } from 'light-react-grid';
+import { GridSystem } from 'light-react-grid';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './main.scss';
+import { gridSettings } from './constants';
 
 ReactDOM.render(
-  <GridSystem
-    useGridHelper={true}
-    settings={{
-      ...GRID_SETTINGS,
-      gridHelperColumnColor: 'rgba(0, 0, 0, .05)',
-      gridHelperMargins: {
-        sm: '5%',
-        md: '5%',
-        lg: '5%',
-      },
-    }}
-  >
+  <GridSystem useGridHelper={true} settings={gridSettings}>
     <App />
   </GridSystem>,
   document.getElementById('root')
