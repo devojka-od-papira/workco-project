a7c2940aa0615146e4307f225039aac14653a2ce
Marko Sutija <markosutija@gmail.com>
2022-08-11 16:24:51 +0200

    primer ispravnog fonta

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from "react";
-import "light-react-grid/dist/index.css";
-import { useBreakpoint, Grid, Column } from "light-react-grid";
-import Header from "./components/header/Header";
-import Container from "./components/container/Container"
+import React from 'react';
+import 'light-react-grid/dist/index.css';
+import Header from './components/header/Header';
+import Container from './components/container/Container';
 
 function App() {
-  const breakpoint = useBreakpoint();
-
   return (
     <Container>
       <Header />
     </Container>
   );
-};
+}
 
 export default App;
