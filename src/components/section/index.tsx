1a39c39a45516780368a4b7059d3dce8963eb41d
Bojana Bojic <bojic@work.co>
2022-09-09 12:22:02 +0100

    Diveder has unique color

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Column, Grid } from 'light-react-grid';
+import cx from 'classnames';
 import styles from './Section.module.scss';
 import Container from '../container';
 
@@ -11,7 +12,14 @@ const Section: React.FC<SectionProps> = ({ title, children }) => (
   <Container>
     <Grid>
       <Column size={{ lg: 2 }} className={styles.section}>
-        <h3 className={styles.title}>{title}</h3>
+        <h3
+          className={cx(styles.title, {
+            [styles.blue]: title === 'News',
+            [styles.purple]: title === 'Classes',
+          })}
+        >
+          {title}
+        </h3>
       </Column>
     </Grid>
     {children}
