e56017e3ab3bdeedac3b524cae58b6b06269382c
Bojana Bojic <bojic@work.co>
2022-09-12 18:10:57 +0200

    Fixed code style

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Column, Grid } from 'light-react-grid';
-import cx from 'classnames';
 import styles from './Section.module.scss';
 import Container from '../container';
 
@@ -13,7 +12,7 @@ const Section: React.FC<SectionProps> = ({ title, children, classNames }) => (
   <Container>
     <Grid className={classNames}>
       <Column size={{ lg: 2 }} className={styles.section}>
-        <h3 className={cx(styles.title, { classNames })}>{title}</h3>
+        <h3 className={styles.title}>{title}</h3>
       </Column>
     </Grid>
     {children}
