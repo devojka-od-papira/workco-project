4600b3bb2fa7d884159e0ec7038ab420023c7292
Bojana Bojic <bojic@work.co>
2022-09-15 14:43:59 +0200

    Fix conflicts

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,13 +6,11 @@ import styles from './footer.module.scss';
 const Footer: React.FC = () => (
   <>
     <div className={styles.noContent} />
-    <footer>
-      <div className={styles.content}>
-        <Container>
-          <h3 className={styles.title}>Art school</h3>
-          <p className={styles.description}>127a, Painting street - USA</p>
-        </Container>
-      </div>
+    <footer className={styles.content}>
+      <Container>
+        <h3 className={styles.title}>Art school</h3>
+        <p className={styles.description}>127a, Painting street - USA</p>
+      </Container>
     </footer>
   </>
 );
