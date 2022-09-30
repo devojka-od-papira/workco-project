15f05738e578ffc2d243461f3ee2cabf1137b7d4
Bojana Bojic <bojic@work.co>
2022-09-30 14:18:43 +0200

    Add logo image

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './Logo.module.scss';
+import ArtLogo from '../../assets/images/art-school-logo.svg';
 
-const Logo: React.FC = () => <div className={styles.logo} />;
+const Logo: React.FC = () => (
+  <img className={styles.logo} src={ArtLogo} alt="Logo" />
+);
 
 export default Logo;
