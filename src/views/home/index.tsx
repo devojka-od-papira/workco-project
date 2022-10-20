604d6dc44ec59c6ffe1e2488925b4682f18df30e
Bojana Bojic <bojic@work.co>
2022-10-20 16:56:22 +0200

    Fix git conflict

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,31 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import 'light-react-grid/dist/index.css';
 import News from '../../components/news';
 import Classes from '../../components/classes';
 
-const Home = (): ReactElement => (
-  <>
-    <News />
-    <Classes />
-  </>
-);
+const Home = (): ReactElement => {
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch('api/data.json ');
+        if (response.ok) {
+          const data = await response.json();
+          console.log('data', data);
+        }
+      } catch (err) {
+        console.log('error', err);
+      }
+    };
+
+    fetchNews();
+  }, []);
+  // const loadNews = () => {};
+  return (
+    <>
+      <News />
+      <Classes />
+    </>
+  );
+};
 
 export default Home;
